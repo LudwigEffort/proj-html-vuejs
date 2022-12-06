ba8d9e83c0f0807562e092b0f4a204cc404259d7
LudwigEffort <dr.feynman65@gmail.com>
2022-12-07 00:01:02 +0100

    start countdown

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,5 +1,36 @@
 <template>
-  <main />
+  <main>
+    <div class="countdown_container">
+      <div class="container container-lg text-center py-5">
+        <div class="row row-cols-auto justify-content-center gx-5">
+          <div class="col">
+            <div class="countdown_card">
+              <h4>3399</h4>
+              <p>Days</p>
+            </div>
+          </div>
+          <div class="col">
+            <div class="countdown_card">
+              <h4>17</h4>
+              <p>Days</p>
+            </div>
+          </div>
+          <div class="col">
+            <div class="countdown_card">
+              <h4>28</h4>
+              <p>Days</p>
+            </div>
+          </div>
+          <div class="col">
+            <div class="countdown_card">
+              <h4>17</h4>
+              <p>Days</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
@@ -8,6 +39,14 @@ export default {
 };
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.countdown_container {
+  background-image: url(../assets/img/coutdown-bg1.jpg);
+}
+.countdown_card {
+  background-color: black;
+  color: white;
+  border: 2px solid red;
+  padding: 1rem 2rem;
+}
 </style>
