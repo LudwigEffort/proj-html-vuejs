b63916a7bd70f564b8704eb24f3559aa17c0898a
LudwigEffort <dr.feynman65@gmail.com>
2022-12-06 11:08:02 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <h1>Work started!</h1>
+</div>
+</template>
+
+<script>
+
+export default {
+
+};
+</script>
+
+<style lang="scss">
+@import "~bootstrap/scss/bootstrap";
+</style>
