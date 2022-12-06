0b56f6c477c692d91715b8f9db183680ed6f177f
LudwigEffort <dr.feynman65@gmail.com>
2022-12-06 18:14:13 +0100

    add navbar

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -1,6 +1,88 @@
 <template>
   <header>
-    <h1>This is the header...</h1>
+    <nav class="navbar navbar-expand-lg bg-light">
+      <div class="container-lg blue">
+        <a
+          class="navbar-brand"
+          href="#"
+        >The Keynote</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon" />
+        </button>
+        <div
+          id="navbarNav"
+          class="collapse navbar-collapse justify-content-end"
+        >
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a
+                class="nav-link active"
+                aria-current="page"
+                href="#"
+              >Home</a>
+            </li>
+            <li class="nav-item">
+              <a
+                class="nav-link active"
+                aria-current="page"
+                href="#"
+              >Pages</a>
+            </li>
+            <li class="nav-item">
+              <a
+                class="nav-link active"
+                aria-current="page"
+                href="#"
+              >Program</a>
+            </li>
+            <li class="nav-item">
+              <a
+                class="nav-link active"
+                aria-current="page"
+                href="#"
+              >Tickets</a>
+            </li>
+            <li class="nav-item">
+              <a
+                class="nav-link active"
+                aria-current="page"
+                href="#"
+              >Speakers</a>
+            </li>
+            <li class="nav-item">
+              <a
+                class="nav-link active"
+                aria-current="page"
+                href="#"
+              >Papers</a>
+            </li>
+            <li class="nav-item">
+              <a
+                class="nav-link active"
+                aria-current="page"
+                href="#"
+              >Blog</a>
+            </li>
+            <li class="nav-item">
+              <a
+                class="nav-link active"
+                aria-current="page"
+                href="#"
+              >Shortcodes</a>
+            </li>
+          </ul>
+          <span>icon</span>
+        </div>
+      </div>
+    </nav>
   </header>
 </template>
 
